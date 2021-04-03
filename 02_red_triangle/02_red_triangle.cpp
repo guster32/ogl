322684b51babcbbe54d33dd2987b8ad8147f8463
Gustavo Branco <guster32@gmail.com>
2021-04-03 06:14:07 -0700

    Adds glsl linter extension

--- 02_red_triangle/02_red_triangle.cpp
+++ 02_red_triangle/02_red_triangle.cpp
@@ -63,7 +63,7 @@ int main(void)
     glBindVertexArray(VertexArrayID);
 
     // Create and compile our GLSL program from the shaders
-    GLuint programID = LoadShaders("SimpleVertexShader.glsl", "SimpleFragmentShader.glsl");
+    GLuint programID = LoadShaders("SimpleVertexShader.vs.glsl", "SimpleFragmentShader.fs.glsl");
 
     static const GLfloat g_vertex_buffer_data[] = {
         -1.0f,
