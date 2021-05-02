a7c98bdecfad02606171ff04be1f803b1c0045c5
Gustavo Branco <guster32@gmail.com>
2021-05-02 11:14:51 -0700

    Saving progress on tutorial 05

--- 05_textured_cube/05_textured_cube.cpp
+++ 05_textured_cube/05_textured_cube.cpp
@@ -1,4 +1,24 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
 
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+// Include GLM
+#include <glm/glm.hpp>
+using namespace glm;
+
+GLuint loadBMP_custom(const char * imagepath)
+{
+    // Data from header of BMP file
+    unsigned char header[54]; // Each BMP file begins with 54 bytes header
+
+}
 
 int main( void )
 {
