3a914a5a737efe334c3950e53ea380f2e50c792d
Gustavo Branco <guster32@gmail.com>
2024-09-02 14:17:55 -0700

    Adds sample SDL2 window

---
+++ 00_sdl2/00_sdl2.cpp
@@ -0,0 +1,90 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLEW
+#include <GL/glew.h>
+
+// Include SDL2
+#include <SDL2/SDL.h>
+
+// Include GLM
+#include <glm/glm.hpp>
+using namespace glm;
+
+int main(void)
+{
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "Failed to initialize SDL: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // Set OpenGL attributes
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+    // Create an SDL window
+    SDL_Window* window = SDL_CreateWindow("Tutorial 01", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    if (!window) {
+        fprintf(stderr, "Failed to create SDL window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
+
+    // Create an OpenGL context
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+    if (!glContext) {
+        fprintf(stderr, "Failed to create OpenGL context: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    // Initialize GLEW
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        SDL_GL_DeleteContext(glContext);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    // Enable V-Sync
+    if (SDL_GL_SetSwapInterval(1) < 0) {
+        fprintf(stderr, "Failed to set V-Sync: %s\n", SDL_GetError());
+    }
+
+    // Dark blue background
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        // Handle events
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            } else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE) {
+                running = false;
+            }
+        }
+
+        // Clear the screen
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap buffers
+        SDL_GL_SwapWindow(window);
+    }
+
+    // Cleanup
+    SDL_GL_DeleteContext(glContext);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
