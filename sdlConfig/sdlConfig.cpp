f5bdad04556434ee86cd5d3a46c6bfdee14bfe43
Gustavo Branco <guster32@gmail.com>
2024-09-02 18:52:59 -0700

    Manually set buffer

--- sdlConfig/sdlConfig.cpp
+++ sdlConfig/sdlConfig.cpp
@@ -51,9 +51,14 @@ int main(int argc, char* argv[]) {
     SDL_LogSetAllPriority(SDL_LOG_PRIORITY_DEBUG);
 
     // Set some OpenGL attributes before creating the window
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 0);  // No alpha channel
+    SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 24);  // 8 bits each for R, G, B
 
     // Print the attributes SDL will use to request an EGLConfig
     print_desired_sdl_opengl_config();
@@ -62,8 +67,8 @@ int main(int argc, char* argv[]) {
     SDL_Window* window = SDL_CreateWindow("SDL OpenGL Config",
                                           SDL_WINDOWPOS_CENTERED,
                                           SDL_WINDOWPOS_CENTERED,
-                                          800, 600,
-                                          SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+                                          0, 0,
+                                          SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN_DESKTOP);
     if (!window) {
         fprintf(stderr, "Failed to create SDL window: %s\n", SDL_GetError());
         SDL_Quit();
