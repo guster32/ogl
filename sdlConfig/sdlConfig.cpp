043cd6e2c1306cdb5722a5c3773e38a547a55175
Gustavo Branco <guster32@gmail.com>
2024-09-02 19:11:49 -0700

    Set Gl context version to 3.1

--- sdlConfig/sdlConfig.cpp
+++ sdlConfig/sdlConfig.cpp
@@ -48,7 +48,7 @@ int main(int argc, char* argv[]) {
     SDL_LogSetAllPriority(SDL_LOG_PRIORITY_DEBUG);
 
     // Set some OpenGL attributes before creating the window
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
     SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
@@ -56,9 +56,6 @@ int main(int argc, char* argv[]) {
     SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 0);  // No alpha channel
     SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 24);  // 8 bits each for R, G, B
-
-    // Print the attributes SDL will use to request an EGLConfig
-    print_desired_sdl_opengl_config();
 
     // Create a window with an OpenGL context
     SDL_Window* window = SDL_CreateWindow("SDL OpenGL Config",
