2d81e8d6fabc9867cd8a03518b2fce3f38fd47be
Gustavo Branco <guster32@gmail.com>
2024-09-02 20:20:29 -0700

    Some adjustments.

--- 00_sdl2/00_sdl2.cpp
+++ 00_sdl2/00_sdl2.cpp
@@ -25,9 +25,12 @@ int main(void)
     // Set OpenGL attributes
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 0);  // No alpha channel
+    SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 24);  // 8 bits each for R, G, B
 
     // Create an SDL window
     SDL_Window* window = SDL_CreateWindow("Tutorial 01", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 0, 0, SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN_DESKTOP);
